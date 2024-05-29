9fe2e7c36f719506a4e3b31767e177ee99e41a16
Thien Y <y.nguyen@xomad.com>
2024-05-29 16:04:13 +0700

    caesar_cipher_cli update learning

--- caesar_cipher_cli/src/main.rs
+++ caesar_cipher_cli/src/main.rs
@@ -1,3 +1,33 @@
+use caesar_cipher_cli::{decrypt, encrypt};
+use clap::Parser;
+
+
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about=None)]
+struct Args {
+    #[arg(short, long)]
+    encrypt: bool,
+
+    #[arg(short, long)]
+    decrypt: bool,
+
+    #[arg(short, long)]
+    message: String,
+
+    #[arg(short, long, default_value="3")]
+    shift: u8,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+    if args.decrypt {
+        println!("{}", decrypt(&args.message, args.shift));
+    }
+    else if args.encrypt {
+        println!("{}", encrypt(&args.message, args.shift));
+    }
+    else {
+        println!("Please specify either --encrypt or --decrypt");
+    }
 }
